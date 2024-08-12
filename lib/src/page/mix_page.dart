60dddb84618e4a938017573e3c7c2856ebdb3e36
Payam Zahedi <pzahedi1995@gmail.com>
2024-08-12 13:15:08 +0200

    refactor: refactor the code for mix page

--- lib/src/page/mix_page.dart
+++ lib/src/page/mix_page.dart
@@ -10,22 +10,14 @@ class MixPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       backgroundColor: ThemeColors.green,
-      body: IconTheme(
-        data: const IconThemeData(color: ThemeColors.green),
-        child: DefaultTextStyle.merge(
-          style: const TextStyle(color: ThemeColors.darkGreen),
-          child: const Center(
-            child: Body(
-              title: 'Mix your M&Ms',
-              description:
-                  'Swipe the slider and choose how many M&Ms you want to mix.',
-              lightColor: ThemeColors.green,
-              darkColor: ThemeColors.darkGreen,
-            ),
-          ),
-        ),
+      body: Body(
+        title: 'Mix your M&Ms',
+        description:
+            'Swipe the slider and choose how many M&Ms you want to mix.',
+        lightColor: ThemeColors.green,
+        darkColor: ThemeColors.darkGreen,
       ),
     );
   }
@@ -57,95 +49,107 @@ class _BodyState extends State<Body> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Text(
-          widget.title,
-          textAlign: TextAlign.center,
-          style: Theme.of(context).textTheme.headlineLarge?.copyWith(
-                color: widget.darkColor,
-              ),
+    return IconTheme(
+      data: IconThemeData(color: widget.lightColor),
+      child: DefaultTextStyle.merge(
+        style: TextStyle(
+          color: widget.darkColor,
         ),
-        Text(
-          widget.description,
-          textAlign: TextAlign.center,
-          style: Theme.of(context).textTheme.titleLarge?.copyWith(
-                color: widget.darkColor,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                widget.title,
+                textAlign: TextAlign.center,
+                style: Theme.of(context).textTheme.headlineLarge?.copyWith(
+                      color: widget.darkColor,
+                    ),
               ),
-        ),
-        const SizedBox(height: 46),
-        SizedBox(
-          width: 800,
-          child: BlocBuilder<MixCubit, MixState>(
-            builder: (context, state) {
-              final loading = state is MixLoading;
+              Text(
+                widget.description,
+                textAlign: TextAlign.center,
+                style: Theme.of(context).textTheme.titleLarge?.copyWith(
+                      color: widget.darkColor,
+                    ),
+              ),
+              const SizedBox(height: 46),
+              SizedBox(
+                width: 800,
+                child: BlocBuilder<MixCubit, MixState>(
+                  builder: (context, state) {
+                    final loading = state is MixLoading;
 
-              return Slider(
-                value: sliderValue,
-                min: 0.0,
-                max: 10.0,
-                divisions: 10,
-                activeColor: widget.darkColor,
-                inactiveColor: Colors.white54,
-                label: sliderValue.round().toString(),
-                onChanged: loading
-                    ? null
-                    : (value) {
-                        setState(() {
-                          sliderValue = value;
-                        });
-                      },
-              );
-            },
+                    return Slider(
+                      value: sliderValue,
+                      min: 0.0,
+                      max: 10.0,
+                      divisions: 10,
+                      activeColor: widget.darkColor,
+                      inactiveColor: Colors.white54,
+                      label: sliderValue.round().toString(),
+                      onChanged: loading
+                          ? null
+                          : (value) {
+                              setState(() {
+                                sliderValue = value;
+                              });
+                            },
+                    );
+                  },
+                ),
+              ),
+              const SizedBox(height: 46),
+              BlocBuilder<MixCubit, MixState>(
+                builder: (context, state) {
+                  log('State: $state');
+
+                  final loading = state is MixLoading;
+
+                  return FilledButton(
+                    style: FilledButton.styleFrom(
+                      backgroundColor: widget.darkColor,
+                      foregroundColor: widget.lightColor,
+                    ),
+                    onPressed: loading
+                        ? null
+                        : () {
+                            context
+                                .read<MixCubit>()
+                                .mixCandies(sliderValue.toInt());
+                          },
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(widget.buttonText ?? 'Mix'),
+                        const SizedBox(width: 16),
+                        if (loading)
+                          const SizedBox(
+                            width: 48,
+                            height: 48,
+                            child: Center(
+                              child: CircularProgressIndicator(
+                                strokeWidth: 4,
+                                valueColor: AlwaysStoppedAnimation<Color>(
+                                    Colors.black26),
+                              ),
+                            ),
+                          )
+                        else
+                          const Icon(
+                            Icons.east_rounded,
+                            size: 48,
+                          ),
+                      ],
+                    ),
+                  );
+                },
+              ),
+            ],
           ),
         ),
-        const SizedBox(height: 46),
-        BlocBuilder<MixCubit, MixState>(
-          builder: (context, state) {
-            log('State: $state');
-
-            final loading = state is MixLoading;
-
-            return FilledButton(
-              style: FilledButton.styleFrom(
-                backgroundColor: widget.darkColor,
-                foregroundColor: widget.lightColor,
-              ),
-              onPressed: loading
-                  ? null
-                  : () {
-                      context.read<MixCubit>().mixCandies(sliderValue.toInt());
-                    },
-              child: Row(
-                mainAxisSize: MainAxisSize.min,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(widget.buttonText ?? 'Mix'),
-                  const SizedBox(width: 16),
-                  if (loading)
-                    const SizedBox(
-                      width: 48,
-                      height: 48,
-                      child: Center(
-                        child: CircularProgressIndicator(
-                          strokeWidth: 4,
-                          valueColor:
-                              AlwaysStoppedAnimation<Color>(Colors.black26),
-                        ),
-                      ),
-                    )
-                  else
-                    const Icon(
-                      Icons.east_rounded,
-                      size: 48,
-                    ),
-                ],
-              ),
-            );
-          },
-        ),
-      ],
+      ),
     );
   }
 }
